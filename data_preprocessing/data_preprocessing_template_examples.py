afd1db81abdc506906296692a7563d762603fac6
Ryan McDougall <rp.mcdougall@gmail.com>
2019-05-16 20:39:14 -0500

    keep examples and create new file to contain template

--- data_preprocessing/data_preprocessing_template_examples.py
+++ data_preprocessing/data_preprocessing_template_examples.py
@@ -9,9 +9,28 @@ from sklearn.model_selection import train_test_split
 # Import Data
 dataset = pd.read_csv('data/Data.csv')
 
+
 # Load data set
 feature_matrix = dataset.iloc[:, :-1].values
 dep_var_vec = dataset.iloc[:, 3].values
+
+# Handle missing data
+# imputer = SimpleImputer(missing_values=np.nan, strategy="mean")
+# imputer = imputer.fit(feature_matrix[:, 1:3])
+# feature_matrix[:, 1:3] = imputer.transform(feature_matrix[:, 1:3])
+
+"""
+Encode Categorical Variables using One Hot Encoding
+"""
+# # Country
+# label_encoder_feat = LabelEncoder()
+# feature_matrix[:, 0] = label_encoder_feat.fit_transform(feature_matrix[:, 0])
+# ohe = OneHotEncoder(categorical_features=[0])
+# feature_matrix = ohe.fit_transform(feature_matrix).toarray()
+#
+# # Purchased
+# label_encoder_dep = LabelEncoder()
+# dep_var_vec = label_encoder_dep.fit_transform(dep_var_vec)
 
 """
 Split data into training and test set
